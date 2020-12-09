41f97fc31f2ee94b2b07c4c7329de7bcad7b467e
fishpenguin <kun.yu@zilliz.com>
2020-12-09 19:08:36 +0800

    Change status proto to remove enum ErrorCode
    Signed-off-by: fishpenguin <kun.yu@zilliz.com>

--- milvus/grpc/go_gen.sh
+++ milvus/grpc/go_gen.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-/home/yukun/test/milvus/core/cmake-build-debug/grpc_ep-prefix/src/grpc_ep/bins/opt/protobuf/protoc -I . milvus.proto --go_out=plugins=grpc:gen
-
-/home/yukun/test/milvus/core/cmake-build-debug/grpc_ep-prefix/src/grpc_ep/bins/opt/protobuf/protoc -I . status.proto --go_out=plugins=grpc:gen
+/home/yukun/backup/milvus/core/cmake-build-debug/grpc_ep-prefix/src/grpc_ep/bins/opt/protobuf/protoc -I . milvus.proto --go_out=plugins=grpc:gen
 
 
