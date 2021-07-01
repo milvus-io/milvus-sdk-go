55d2b47b6daf908d1fb1c8c886a73da93f742a4a
congqixia <congqi.xia@zilliz.com>
2021-07-01 18:18:54 +0800

    Move proto internal (#106)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- scripts/proto_gen_go.sh
+++ scripts/proto_gen_go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPTS_DIR=$(dirname "$0")
 
-PROTO_DIR=$SCRIPTS_DIR/../proto
+PROTO_DIR=$SCRIPTS_DIR/../internal/proto
 
 PROGRAM=$(basename "$0")
 GOPATH=$(go env GOPATH)
@@ -21,11 +21,11 @@ echo "using protoc-gen-go: $(which protoc-gen-go)"
 pushd ${PROTO_DIR}
 
 mkdir -p common
-mkdir -p schema
+mkdir -p server
 mkdir -p milvus
 
 
-protoc --go_out=plugins=grpc,paths=source_relative:./milvus milvus.proto
+protoc --go_out=plugins=grpc,paths=source_relative:./server milvus.proto
 protoc --go_out=plugins=grpc,paths=source_relative:./common common.proto
 protoc --go_out=plugins=grpc,paths=source_relative:./schema schema.proto
 
