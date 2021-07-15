a5837aa8021a959c673c016c97f5cc48cc52f98a
Congqi Xia <congqi.xia@zilliz.com>
2021-07-15 14:30:59 +0800

    Add CreateIndex sync test case
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- client/client_grpc_dd_test.go
+++ client/client_grpc_dd_test.go
@@ -759,12 +759,57 @@ func TestGrpcClientReleasePartitions(t *testing.T) {
 func TestGrpcClientCreateIndex(t *testing.T) {
 	ctx := context.Background()
 	c := testClient(ctx, t)
-	idx := entity.NewGenericIndex("", entity.Flat, map[string]string{
-		"nlist":       "1024",
-		"metric_type": "IP",
+
+	fieldName := `vector`
+	idx, err := entity.NewIndexFlat(entity.IP, 1024)
+	assert.Nil(t, err)
+	if !assert.NotNil(t, idx) {
+		t.FailNow()
+	}
+	mock.setInjection(mCreateIndex, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+		req, ok := raw.(*server.CreateIndexRequest)
+		if !ok {
+			return badRequestStatus()
+		}
+		assert.Equal(t, testCollectionName, req.GetCollectionName())
+		assert.Equal(t, fieldName, req.GetFieldName())
+		return successStatus()
 	})
 
-	assert.Nil(t, c.CreateIndex(ctx, testCollectionName, "vector", idx, true))
+	t.Run("test async create index", func(t *testing.T) {
+		assert.Nil(t, c.CreateIndex(ctx, testCollectionName, fieldName, idx, true))
+	})
+
+	t.Run("test sync create index", func(t *testing.T) {
+		buildTime := rand.Intn(900) + 100
+		start := time.Now()
+		flag := false
+		mock.setInjection(mGetIndexState, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			req, ok := raw.(*server.GetIndexStateRequest)
+			resp := &server.GetIndexStateResponse{}
+			if !ok {
+				s, err := badRequestStatus()
+				resp.Status = s
+				return resp, err
+			}
+			assert.Equal(t, testCollectionName, req.CollectionName)
+
+			resp.State = common.IndexState_InProgress
+			if time.Since(start) > time.Duration(buildTime)*time.Millisecond {
+				resp.State = common.IndexState_Finished
+				flag = true
+			}
+
+			s, err := successStatus()
+			resp.Status = s
+			return resp, err
+		})
+
+		assert.Nil(t, c.CreateIndex(ctx, testCollectionName, fieldName, idx, false))
+		assert.True(t, flag)
+
+		mock.delInjection(mGetIndexState)
+	})
 }
 
 func TestGrpcClientDropIndex(t *testing.T) {
