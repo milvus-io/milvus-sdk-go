2667da38dfcffcfeb8690f24eb4a78fc2a18d4ff
Congqi Xia <congqi.xia@zilliz.com>
2022-01-26 11:13:16 +0800

    Add unit tests for index related apis
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- client/client_grpc_index_test.go
+++ client/client_grpc_index_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	"math/rand"
 	"testing"
 	"time"
@@ -88,33 +89,121 @@ func TestGrpcClientDescribeIndex(t *testing.T) {
 
 	fieldName := "vector"
 
-	mock.setInjection(mDescribeIndex, func(_ context.Context, raw proto.Message) (proto.Message, error) {
-		req, ok := raw.(*server.DescribeIndexRequest)
-		resp := &server.DescribeIndexResponse{}
-		if !ok {
-			s, err := badRequestStatus()
+	t.Run("normal describe index", func(t *testing.T) {
+		mock.setInjection(mDescribeIndex, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			req, ok := raw.(*server.DescribeIndexRequest)
+			resp := &server.DescribeIndexResponse{}
+			if !ok {
+				s, err := badRequestStatus()
+				resp.Status = s
+				return resp, err
+			}
+			assert.Equal(t, fieldName, req.GetFieldName())
+			assert.Equal(t, testCollectionName, req.GetCollectionName())
+			resp.IndexDescriptions = []*server.IndexDescription{
+				{
+					IndexName: "_default",
+					IndexID:   1,
+					FieldName: req.GetFieldName(),
+					Params: entity.MapKvPairs(map[string]string{
+						"nlist":       "1024",
+						"metric_type": "IP",
+					}),
+				},
+			}
+			s, err := successStatus()
 			resp.Status = s
 			return resp, err
-		}
-		assert.Equal(t, fieldName, req.GetFieldName())
-		assert.Equal(t, testCollectionName, req.GetCollectionName())
-		resp.IndexDescriptions = []*server.IndexDescription{
-			{
-				IndexName: "_default",
-				IndexID:   1,
-				FieldName: req.GetFieldName(),
-				Params: entity.MapKvPairs(map[string]string{
-					"nlist":       "1024",
-					"metric_type": "IP",
-				}),
-			},
-		}
-		s, err := successStatus()
-		resp.Status = s
-		return resp, err
+		})
+
+		idxes, err := c.DescribeIndex(ctx, testCollectionName, fieldName)
+		assert.Nil(t, err)
+		assert.NotNil(t, idxes)
 	})
 
-	idxes, err := c.DescribeIndex(ctx, testCollectionName, fieldName)
-	assert.Nil(t, err)
-	assert.NotNil(t, idxes)
+	t.Run("service return errors", func(t *testing.T) {
+		defer mock.delInjection(mDescribeIndex)
+		mock.setInjection(mDescribeIndex, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			resp := &server.DescribeIndexResponse{}
+
+			return resp, errors.New("mocked error")
+		})
+
+		_, err := c.DescribeIndex(ctx, testCollectionName, fieldName)
+		assert.Error(t, err)
+
+		mock.setInjection(mDescribeIndex, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			resp := &server.DescribeIndexResponse{}
+			resp.Status = &common.Status{ErrorCode: common.ErrorCode_UnexpectedError}
+			return resp, nil
+		})
+
+		_, err = c.DescribeIndex(ctx, testCollectionName, fieldName)
+		assert.Error(t, err)
+	})
+}
+
+func TestGrpcGetIndexBuildProgress(t *testing.T) {
+	ctx := context.Background()
+	mock.setInjection(mHasCollection, hasCollectionDefault)
+	mock.setInjection(mDescribeCollection, describeCollectionInjection(t, 0, testCollectionName, defaultSchema()))
+
+	tc := testClient(ctx, t)
+	c := tc.(*grpcClient) // since GetIndexBuildProgress is not exposed
+
+	t.Run("normal get index build progress", func(t *testing.T) {
+		var total, built int64
+
+		mock.setInjection(mGetIndexBuildProgress, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			req, ok := raw.(*server.GetIndexBuildProgressRequest)
+			if !ok {
+				t.FailNow()
+			}
+			assert.Equal(t, testCollectionName, req.GetCollectionName())
+			resp := &server.GetIndexBuildProgressResponse{
+				TotalRows:   total,
+				IndexedRows: built,
+			}
+			s, err := successStatus()
+			resp.Status = s
+			return resp, err
+		})
+
+		total = rand.Int63n(1000)
+		built = rand.Int63n(total)
+		rt, rb, err := c.GetIndexBuildProgress(ctx, testCollectionName, testVectorField)
+		assert.NoError(t, err)
+		assert.Equal(t, total, rt)
+		assert.Equal(t, built, rb)
+	})
+
+	t.Run("service return errors", func(t *testing.T) {
+		defer mock.delInjection(mGetIndexBuildProgress)
+		mock.setInjection(mGetIndexBuildProgress, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			_, ok := raw.(*server.GetIndexBuildProgressRequest)
+			if !ok {
+				t.FailNow()
+			}
+			resp := &server.GetIndexBuildProgressResponse{}
+			return resp, errors.New("mocked error")
+		})
+
+		_, _, err := c.GetIndexBuildProgress(ctx, testCollectionName, testVectorField)
+		assert.Error(t, err)
+
+		mock.setInjection(mGetIndexBuildProgress, func(_ context.Context, raw proto.Message) (proto.Message, error) {
+			_, ok := raw.(*server.GetIndexBuildProgressRequest)
+			if !ok {
+				t.FailNow()
+			}
+			resp := &server.GetIndexBuildProgressResponse{}
+			resp.Status = &common.Status{
+				ErrorCode: common.ErrorCode_UnexpectedError,
+			}
+			return resp, nil
+		})
+		_, _, err = c.GetIndexBuildProgress(ctx, testCollectionName, testVectorField)
+		assert.Error(t, err)
+	})
+
 }
