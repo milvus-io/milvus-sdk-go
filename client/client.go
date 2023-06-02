72007091b6c4f624341a4891399a8639138ffc57
wayblink <anyang.wang@zilliz.com>
2023-06-02 19:14:30 +0800

    Update to support milvus-backup (#464)
    Signed-off-by: wayblink <anyang.wang@zilliz.com>

--- client/client.go
+++ client/client.go
@@ -121,6 +121,9 @@ type Client interface {
 	Insert(ctx context.Context, collName string, partitionName string, columns ...entity.Column) (entity.Column, error)
 	// Flush flush collection, specified
 	Flush(ctx context.Context, collName string, async bool) error
+	// FlushV2 flush collection, specified, return newly sealed segmentIds, all flushed segmentIds of the collection, seal time and error
+	// currently it is only used in milvus-backup(https://github.com/zilliztech/milvus-backup)
+	FlushV2(ctx context.Context, collName string, async bool) ([]int64, []int64, int64, error)
 	// DeleteByPks deletes entries related to provided primary keys
 	DeleteByPks(ctx context.Context, collName string, partitionName string, ids entity.Column) error
 	// Search search with bool expression
