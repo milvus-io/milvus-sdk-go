a9cdfa0b72c68b79b34eb85bf7fee2e480843d90
Congqi Xia <congqi.xia@zilliz.com>
2024-01-30 12:51:24 +0800

    enhance: Suport GPUCagra & GPUBruteForce index type
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- entity/index_test.go
+++ entity/index_test.go
@@ -15,13 +15,12 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenericIndex(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
 	name := fmt.Sprintf("generic_index_%d", rand.Int())
 	gi := NewGenericIndex(name, IvfFlat, map[string]string{
 		tMetricType: string(IP),
@@ -40,4 +39,95 @@ func TestAddRangeFilter(t *testing.T) {
 	params := newBaseSearchParams()
 	params.AddRangeFilter(20)
 	assert.Equal(t, params.Params()["range_filter"], float64(20))
+}
+
+func TestIndexGPUCagra(t *testing.T) {
+	t.Run("index", func(t *testing.T) {
+		index, err := NewIndexGPUCagra(L2, 64, 64)
+		require.NoError(t, err)
+		require.NotNil(t, index)
+
+		assert.Equal(t, "GPUCagra", index.Name())
+		assert.Equal(t, GPUCagra, index.IndexType())
+		assert.False(t, index.SupportBinary())
+
+		params := index.Params()
+
+		metricType, ok := params["metric_type"]
+		require.True(t, ok)
+		assert.Equal(t, string(L2), metricType)
+
+		indexType, ok := params["index_type"]
+		require.True(t, ok)
+		assert.Equal(t, string(GPUCagra), indexType)
+
+		_, err = NewIndexGPUCagra(L2, 32, 64)
+		assert.Error(t, err)
+	})
+
+	t.Run("search_param", func(t *testing.T) {
+		sp, err := NewIndexGPUCagraSearchParam(
+			64,
+			1,
+			0,
+			0,
+			4,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, sp)
+
+		params := sp.Params()
+		itopkSize, ok := params["itopk_size"]
+		require.True(t, ok)
+		assert.EqualValues(t, 64, itopkSize)
+		searchWidth, ok := params["search_width"]
+		require.True(t, ok)
+		assert.EqualValues(t, 1, searchWidth)
+		maxIterations, ok := params["max_iterations"]
+		require.True(t, ok)
+		assert.EqualValues(t, 0, maxIterations)
+		minIterations, ok := params["min_iterations"]
+		require.True(t, ok)
+		assert.EqualValues(t, 0, minIterations)
+		teamSize, ok := params["team_size"]
+		require.True(t, ok)
+		assert.EqualValues(t, 4, teamSize)
+
+		_, err = NewIndexGPUCagraSearchParam(
+			64,
+			1,
+			0,
+			0,
+			3,
+		)
+		assert.Error(t, err)
+	})
+}
+
+func TestIndexGPUBruteForce(t *testing.T) {
+	t.Run("index", func(t *testing.T) {
+		index, err := NewIndexGPUBruteForce(L2)
+		require.NoError(t, err)
+		require.NotNil(t, index)
+
+		assert.Equal(t, "GPUBruteForce", index.Name())
+		assert.Equal(t, GPUBruteForce, index.IndexType())
+		assert.False(t, index.SupportBinary())
+
+		params := index.Params()
+
+		metricType, ok := params["metric_type"]
+		require.True(t, ok)
+		assert.Equal(t, string(L2), metricType)
+
+		indexType, ok := params["index_type"]
+		require.True(t, ok)
+		assert.Equal(t, string(GPUBruteForce), indexType)
+	})
+
+	t.Run("search_param", func(t *testing.T) {
+		sp, err := NewIndexGPUBruteForceSearchParam()
+		assert.NoError(t, err)
+		assert.NotNil(t, sp)
+	})
 }
