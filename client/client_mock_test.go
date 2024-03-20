00e74ab2ff6e3a6ce7e5615f7266ede6c1fb09f3
congqixia <congqi.xia@zilliz.com>
2024-03-20 21:59:00 +0800

    enhance: Bump milvus-proto/go-api to v2.4.0-rc.1 (#682)
    Sync proto version to v2.4.0-rc.1 to support new features
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- client/client_mock_test.go
+++ client/client_mock_test.go
@@ -1026,3 +1026,7 @@ func (m *MockServer) HybridSearch(ctx context.Context, req *milvuspb.HybridSearc
 	status, err := SuccessStatus()
 	return &milvuspb.SearchResults{Status: status}, err
 }
+
+func (m *MockServer) UpdateResourceGroups(_ context.Context, _ *milvuspb.UpdateResourceGroupsRequest) (*commonpb.Status, error) {
+	return nil, errors.New("not implemented")
+}
