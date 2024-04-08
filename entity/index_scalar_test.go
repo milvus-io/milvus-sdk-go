6bb1f79d494ccb35347b785421e54bccb588c42a
congqixia <congqi.xia@zilliz.com>
2024-04-08 17:27:10 +0800

    fix: Pass index type for Scalar indexes (#696)
    Related to #694 #695
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- entity/index_scalar_test.go
+++ entity/index_scalar_test.go
@@ -10,8 +10,16 @@ func TestScalarIndex(t *testing.T) {
 	oldScalarIdx := NewScalarIndex()
 
 	assert.EqualValues(t, "", oldScalarIdx.IndexType(), "use AUTO index when index type not provided")
+	_, has := oldScalarIdx.Params()[tIndexType]
+	assert.False(t, has)
 
 	idxWithType := NewScalarIndexWithType(Sorted)
 
 	assert.EqualValues(t, Sorted, idxWithType.IndexType())
+	assert.EqualValues(t, Sorted, idxWithType.Params()[tIndexType])
+
+	idxWithType = NewScalarIndexWithType(Trie)
+
+	assert.EqualValues(t, Trie, idxWithType.IndexType())
+	assert.EqualValues(t, Trie, idxWithType.Params()[tIndexType])
 }
